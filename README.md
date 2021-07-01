594037cda52615df4882d54faffaeb7b08c95449
aashishpeepra <ironhack082327@gmail.com>
2021-07-01 22:46:13 +0530

    Initializing

---
+++ README.md
@@ -0,0 +1,5 @@
+# Strapi application
+
+pass- r7jQrR2v8trXcgR
+
+A quick description of your strapi application
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a09634e7-e6cf-439f-aecd-c0c48c714a3b'
+};
